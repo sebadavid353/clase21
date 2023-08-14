40251e830e93f861283c3c3699392cd7d9485454
sebastian David <sebastiandn@hotmail.com>
2023-08-14 18:57:15 -0300

    se agrega producto en admin

--- project/apps/producto/models.py
+++ project/apps/producto/models.py
@@ -17,14 +17,13 @@ class ProductoCategoria(models.Model):
 class Producto(models.Model):
     categoria = models.ForeignKey(ProductoCategoria, on_delete=models.SET_NULL, blank=True,null=True, verbose_name="categoria")
     nombre = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=150, blank=True, null=True)  
+    descripcion = models.CharField(max_length=150, blank=True, null=True, verbose_name="descripcion")  
     precio = models.DecimalField(max_digits=10, decimal_places=2) 
-
 
 
     class meta:
         verbose_name = 'producto'
         verbose_name_plural = "productos"
 
-    def __str__(self):
-        return self.field_name
+    def __str__(self)-> str:
+        return f'{self.nombre} ${self.precio: .2f}{self.categoria}, {self.descripcion}'
